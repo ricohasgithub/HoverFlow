19f78fa6ac2d855550fb8b1de0b1035090220f20
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-03-13 16:25:32 -0400

    Initial Commit
     - Created a new file called CNN.py which will construct the CNN.h5 file and store it

---
+++ ml/CNN.py
@@ -0,0 +1,13 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import pandas as pd
+
+# Sklearn
+from sklearn.model_selection import train_test_split # Helps with organizing data for training
+from sklearn.metrics import confusion_matrix # Helps present results as a confusion-matrix
