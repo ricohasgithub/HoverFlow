9072c5fecaa85e6bc7558c2861ca3812f368833c
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-03-14 21:40:31 -0400

    Updated Layering Construction
     - Migrated model convolution code from the Keras backend API to the Keras Layers API

--- ml/CNN.py
+++ ml/CNN.py
@@ -12,6 +12,7 @@ class Hand_CNN(keras.Model):
     # Assumes all images are color (RGB)
     def __init__ (self, input_size, heatmap_size, stages, joints, is_training=True):
         self.stages = stages
+        self.input_size = input_size
         self.stage_heatmap = []
         self.stage_loss = [0 for _ in range(stages)]
         self.total_loss = 0
@@ -44,22 +45,24 @@ class Hand_CNN(keras.Model):
     # This function initializes a new Hand Gesture CNN based on the constructor arguments
     def build_model(self):
         # Begin model layering construction procedure
-        self.layer1 = keras.backend.conv2d(inputs = self.input_images,
-                                            filters = 64,
-                                            kernel_size = [3, 3],
-                                            strides = [1, 1],
-                                            padding = 'same',
-                                            activation = tf.nn.relu,
-                                            kernel_initializer = tf.contrib.layers.xavier_initializer(),
-                                            name = 'sub_conv1')
-        self.layer2 = keras.backend.conv2d(inputs = self.layer1,
-                                         filters = 64,
-                                         kernel_size = [3, 3],
-                                         strides = [1, 1],
-                                         padding = 'same',
-                                         activation = tf.nn.relu,
-                                         kernel_initializer = tf.contrib.layers.xavier_initializer(),
-                                         name = 'sub_conv2')
+        model = Sequential()
+        
+        model.add(keras.layers.Conv2D(input_shape = (self.input_size, self.input_size, 3),
+                                                             filters = 64,
+                                                             kernel_size = [3,3],
+                                                             strides = [1, 1],
+                                                             activation = keras.activations.relu,
+                                                             kernel_initializer = keras.initializers.glorot_normal
+                                                            ))
+
+        model.add(keras.layers.Conv2D(filters = 64,
+                                      kernel_size = [3, 3],
+                                      strides = [1, 1],
+                                      activation = keras.activations.relu,
+                                      kernel_initializer = keras.initializers.glorot_normal,
+                                     ))
+
+        self.model = model
         
 
     def load_model_weights(self, config):
